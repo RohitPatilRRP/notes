4a4e43da007e1cf0e116c1671e78f28c6bb73fba
RohitPatilRRP <rohit.patil.rrp16@gmail.com>
2019-07-04 10:36:15 +0530

    server

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const dbConfig = require('./config/database.config.js');
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+
+//connection
+mongoose.connect(dbConfig.url, {
+    useNewUrlParser: true
+}).then(() => {
+    console.log("connected");    
+}).catch(err => {
+    console.log(err);
+    process.exit();
+});
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
+
+app.get('/', (req, res) => {
+    res.json({"message": "Welcome to application."});
+});
+
+
+app.listen(3000, () => {
+    console.log("Server is listening on port 3000");
+});
